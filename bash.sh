e0ca611c9b9993704962562f7ddee68fe420f45e
k400w <58237519+k400w@users.noreply.github.com>
2021-12-05 08:18:13 +0200

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
+./xmrig -a ghostrider --url stratum-eu.rplant.xyz:17056 --tls --user RB6ZKJbkkmPaMTAwRk8T92iED67bRYKDuj.k501w --pass password=Coterr33917 --cpu-max-threads-hint 75
